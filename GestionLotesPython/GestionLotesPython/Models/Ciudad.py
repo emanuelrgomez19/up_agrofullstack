c2909e2c12980865742ef4f22904b1be107601f8
LuisOscarGonzalez <lgonzalez@heinlein.com.ar>
2021-05-07 22:17:05 -0300

    modelos con propiedades y relaciones

--- GestionLotesPython/GestionLotesPython/Models/Ciudad.py
+++ GestionLotesPython/GestionLotesPython/Models/Ciudad.py
@@ -1,4 +1,19 @@
-class Ciudad(object):
+from flask import Flask
+from sqlal import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Decimal, Float, ForeignKey
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+app.debug = true
+Base = declarative_base()
+
+class Ciudad(Base):
     """description of class"""
-
-
+    __tablename__ = 'ciudad'
+    idCiudad = Column(Integer(), prymary_key=True)
+    Nombre = Column(String(100))
+    idProvincia = Column(Integer,ForeignKey('provincia.idProvincia'))
+    provincia = relationship(Provincia,backref=backref('ciudadas', uselist=True, cascade='delete,all'))
+    
